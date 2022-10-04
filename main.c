97169cdf4e9c287b63095fb49a109e187193737d
Azret Ulbashev <azret1102@gmail.com>
2022-10-04 23:24:38 +0300

    Give a personalized hello

--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 
 int main(int argc, char **argv)
 {
+	char name[255];
 
-	printf("Hello world!\n");
+	printf("Enter your name: ");
+	fgets(name, 255, stdin);
+	printf("Hello %s!\n", name);
 	return 0;
 }
